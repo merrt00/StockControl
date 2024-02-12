a125b5c848d56b55157b4474c2230c9fa2efefe3
Mert Sen <mert.senn21@gmail.com>
2024-02-12 22:50:34 +0300

    ProjectFinal

---
+++ Stok Girişi/Stok Girişi/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace Stok_Girişi
+{
+    partial class STOKGİRİŞ
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(STOKGİRİŞ));
+            this.txtVeri = new System.Windows.Forms.TextBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.lstBVeriler = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // txtVeri
+            // 
+            this.txtVeri.BackColor = System.Drawing.Color.GhostWhite;
+            this.txtVeri.Location = new System.Drawing.Point(48, 86);
+            this.txtVeri.Name = "txtVeri";
+            this.txtVeri.Size = new System.Drawing.Size(132, 20);
+            this.txtVeri.TabIndex = 0;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(78, 112);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstBVeriler
+            // 
+            this.lstBVeriler.BackColor = System.Drawing.Color.GhostWhite;
+            this.lstBVeriler.FormattingEnabled = true;
+            this.lstBVeriler.Location = new System.Drawing.Point(48, 192);
+            this.lstBVeriler.Name = "lstBVeriler";
+            this.lstBVeriler.Size = new System.Drawing.Size(132, 225);
+            this.lstBVeriler.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Eklenecek Ürün";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(45, 176);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(90, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Depodaki Ürünler";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // STOKGİRİŞ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(236, 450);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBVeriler);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.txtVeri);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "STOKGİRİŞ";
+            this.Text = "STOK GİRİŞ";
+            this.TransparencyKey = System.Drawing.SystemColors.Window;
+            this.Load += new System.EventHandler(this.STOKGİRİŞ_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtVeri;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.ListBox lstBVeriler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Stok Girişi/Stok Girişi/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stok_Girişi
+{
+    public partial class STOKGİRİŞ : Form
+    {
+        int i = 0;
+        string[] dizi = new string[10034];
+        public STOKGİRİŞ()
+        {
+            InitializeComponent();
+        }
+
+        private void STOKGİRİŞ_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (i < 100)
+            {
+                dizi[i] = txtVeri.Text;
+                lstBVeriler.Items.Add(dizi[i]);
+                i++;
+                txtVeri.Clear();
+                txtVeri.Focus();
+            }
+
+        }
+    }
+}
